ecc5f035b863e7c9c666e94dfa5edc5f1edd44a9
Adriana <aviviano@me.com>
2025-01-15 14:07:00 -0500

    excersize1

---
+++ programming1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+int temp;
+printf("Please enter the temperature");
+scanf("%d", &temp);
+if(temp > 75)
+{
+    printf("hi");
+}else{
+    printf("bye");
+}
+
+return 1;
+}
